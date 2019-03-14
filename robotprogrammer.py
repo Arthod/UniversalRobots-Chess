5d9e68e35054f90a64450c48353f78d34c785f7b
Arthod <32404077+Arthod@users.noreply.github.com>
2019-03-14 14:38:35 +0100

    3-14-2019  -- 2 moduler
    Opdatering for robot

---
+++ robotprogrammer.py
@@ -0,0 +1,143 @@
+import socket 
+import time
+
+class Robot_programmer():
+
+    def __init__(self):
+        #Socket til at sende kommandoer til robotten
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.s.settimeout(10)
+        self.connected = False
+        
+        self.top_z = 245/1000.0
+        self.bottom_z = 50/1000.0
+
+    def connect(self, ip='10.130.58.11'):
+        self.TCP_IP = ip
+        TCP_PORT = 30002
+        BUFFER_SIZE = 1024
+
+        try:
+            #print("Opening IP Address" + TCP_IP)
+            self.s.connect((self.TCP_IP, TCP_PORT))
+            response = self.s.recv(BUFFER_SIZE)
+            self.connected = True
+        except socket.error:
+            print("Socket error")
+            self.s.close()
+
+    def move_home(self):
+        if self.connected:
+            #Prædefineret home-position:
+            #(Når vi skal sende en streng til robotten,
+            # skal den konverteres til et bytearrayself.
+            # derfor står der b' foran strengen.)
+            self.s.send(b'  movej([0,-1.5708, 1.5708, -1.5708, -1.5708, 0])\n')
+
+    def move_xyz(self, x, y, z):
+        if self.connected:
+            self.s.send(b'def move_xyz():\n')
+            self.send_socket_move_xyz(x, y, z)
+
+            self.s.send(b'end\n')
+            
+    def move_positions(self, pos, z):
+        if self.connected:
+            self.s.send(b'def myProg():\n')
+            
+            self.s.send(b'  var_1=get_actual_tcp_pose()\n')
+            st = '  var_1[2] = {}\n'.format(z/1000.0)
+            self.s.send(bytearray(st,'utf8'))
+            
+            for i in range(len(pos)):
+                x = pos[i][0]-487.0
+                y = pos[i][1]-107.0
+                
+                #81.8
+                
+                #x = -487.0
+                #y = -107.0
+            
+                self.s.send(b'  var_1=get_actual_tcp_pose()\n')
+                st = '  var_1[0] = {}\n'.format(x/1000.0)
+                self.s.send(bytearray(st,'utf8'))
+                st = '  var_1[1] = {}\n'.format(y/1000.0)
+                self.s.send(bytearray(st,'utf8'))
+                st = '  var_1[2] = {}\n'.format(z/1000.0)
+                self.s.send(bytearray(st,'utf8'))
+                self.s.send(b'  movel(var_1, r = 0.002)\n')
+                self.s.send(bytearray(st,'utf8'))
+            self.s.send(b'  movej([0,-1.5708, 1.5708, -1.5708, -1.5708, 0])\n')
+            self.s.send(b'end\n')
+            
+    def open_gripper(self):
+        TCP_PORT = 29999
+        BUFFER_SIZE = 1024
+
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.settimeout(10)
+        try:
+        	print("Opening IP Address" + self.TCP_IP)
+        	s.connect((self.TCP_IP, TCP_PORT))
+        	response = s.recv(BUFFER_SIZE)
+        except socket.error:
+        	print("Socket error")
+        	s.close()
+        
+        s.send(b"load /programs/opengrip.urp\n")
+        s.send(b"play\n")
+
+        s.close()
+
+    def close_gripper(self):
+        TCP_PORT = 29999
+        BUFFER_SIZE = 1024
+
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.settimeout(10)
+        try:
+        	print("Opening IP Address" + self.TCP_IP)
+        	s.connect((self.TCP_IP, TCP_PORT))
+        	response = s.recv(BUFFER_SIZE)
+        except socket.error:
+        	print("Socket error")
+        	s.close()
+
+        s.send(b"load /programs/closegrip.urp\n")
+        s.send(b"play\n")
+
+        s.close()
+
+    def move_piece(self, from_x, from_y, to_x, to_y):
+        self.s.send(b'def move_piece():\n')
+        
+    #Go to from_position
+        self.send_socket_move_xyz(from_x, from_y, self.top_z) #Go over
+        self.send_socket_move_xyz(from_x, from_y, self.bottom_z) #Go down
+
+    #Grab piece, close gripper
+        #CLOSE HERE
+        time.sleep(5)
+        self.send_socket_move_xyz(from_x, from_y, self.top_z) #Go up
+
+    #Go to to_position
+        self.send_socket_move_xyz(to_x, to_y, self.top_z)
+        self.send_socket_move_xyz(to_x, to_y, self.bottom_z)
+
+    #Put piece, open gripper
+        #OPEN HERE
+        self.send_socket_move_xyz(to_x, to_y, self.top_z) #Go up
+
+        self.s.send(b'end\n')
+
+    def send_socket_move_xyz(self, x, y, z):
+        self.s.send(b'  var_1=get_actual_tcp_pose()\n')
+		
+        st = '  var_1[0] = {}\n'.format(x)
+        self.s.send(bytearray(st,'utf8'))
+        st = '  var_1[1] = {}\n'.format(y)
+        self.s.send(bytearray(st,'utf8'))
+        st = '  var_1[2] = {}\n'.format(z)
+        self.s.send(bytearray(st,'utf8'))
+        self.s.send(b'  movel(var_1)\n')
+        self.s.send(bytearray(st,'utf8'))
--- skak.py
+++ skak.py
@@ -3,6 +3,9 @@ import chess.engine
 import pygame as pg
 import numpy as np
 import sys
+from rTData import RTData
+from robotprogrammer import Robot_programmer
+
 
 class Main:
     def __init__(self):
@@ -13,6 +16,17 @@ class Main:
         self.whose_move = True #hvis tur det er. Hvis den er initialiseret som False, er hvid AI. Ellers er sort AI.
 
         #Robotprogrammer
+        self.robotprogrammer = Robot_programmer()
+        self.robotprogrammer.connect("10.130.58.11")
+        
+        self.robot = RTData()
+        #Robot 1 : 1 0 . 1 3 0 . 5 8 . 1 1
+        #Robot 2 : 1 0 . 1 3 0 . 5 8 . 1 2
+        #Robot 3 : 1 0 . 1 3 0 . 5 8 . 1 3
+        #Robot 4 : 1 0 . 1 3 0 . 5 8 . 1 4
+        self.robot.connect("10.130.58.11", False)
+        self.robotprogrammer.move_home()
+        #self.robotprogrammer.open_gripper()
 
         #Pygame
         pg.init()
@@ -31,18 +45,39 @@ class Main:
         columns = ["a", "b", "c", "d", "e", "f", "g", "h"]
         rows = ["8", "7", "6", "5", "4", "3", "2", "1"]
 
+        ready = True
+        timer = 0
         while not done:
             #Init
             for event in pg.event.get():
                 if event.type == pg.QUIT:
                     done = True
-                    engine.quit()
             pressed = pg.key.get_pressed()
 
+            timer += 1
+            if timer > 60:
+                ready = True
+                timer = 0
+
+            #Buttons
+            mouse = pg.mouse.get_pos()
+            if pg.mouse.get_pressed()[0] and ready == True:
+                if mouse[0] > 560+40 and mouse[0] < 560+40+100:
+                    if mouse[1] > 0 and mouse[1] < 50:
+                        self.robotprogrammer.move_home()
+                        print("Button home")
+                    if mouse[1] > 100 and mouse[1] < 150:
+                        self.robotprogrammer.open_gripper()
+                        print("Button open gripper")
+                    if mouse[1] > 200 and mouse[1] < 250:
+                        self.robotprogrammer.close_gripper()
+                        print("Button close gripper")
+                    ready = False
+                    timer = 0
 
             #AI to move
             if not self.whose_move:
-                result = engine.play(self.board, chess.engine.Limit(time=0.01))
+                result = engine.play(self.board, chess.engine.Limit(time=15))
                 self.play_move(result.move, True)
                 self.whose_move = True
 
@@ -72,6 +107,37 @@ class Main:
             self.draw(pg)
 
     def play_move(self, move, computer):
+        def move_to_coordinates(the_move):
+            from_coordinate = (char_to_int(the_move[0]), int(the_move[1]))
+            to_coordinate = (char_to_int(the_move[2]), int(the_move[3]))
+            return (from_coordinate, to_coordinate)
+
+        def char_to_int(char):
+            if char == "a":
+                return 1
+            if char == "b":
+                return 2
+            if char == "c":
+                return 3
+            if char == "d":
+                return 4
+            if char == "e":
+                return 5
+            if char == "f":
+                return 6
+            if char == "g":
+                return 7
+            if char == "h":
+                return 8
+
+        def map_value(value, start1, stop1, start2, stop2):
+            leftSpan = stop1 - start1
+            rightSpan = stop2 - start1
+
+            valueScaled = float(value - start1) / float(leftSpan)
+
+            return start2 + (valueScaled * rightSpan)
+
         if computer:
             self.board.push(move)
         else:
@@ -79,7 +145,22 @@ class Main:
         print(str(self.return_color(self.whose_move)) + " playing " + str(move))
         print(str(self.board.unicode()) + "\n ---------------")
 
-        #Robotprogrammer
+        #Coords To
+        coords_to = move_to_coordinates(str(move))[1]
+        move_to_x = map_value(coords_to[0], 0, 8, 0, 388) - 588
+        move_to_y = map_value(coords_to[1], 0, 8, 0, 388) - 588
+        #self.robotprogrammer.move_xyz(move_to_x/1000.0, move_to_y/1000.0, 245/1000.0)
+
+        #Coords from
+        coords_from = move_to_coordinates(str(move))[0]
+        move_from_x = map_value(coords_from[0], 0, 8, 0, 388) - 588
+        move_from_y = map_value(coords_from[1], 0, 8, 0, 388) - 588
+        
+        self.robotprogrammer.move_piece(move_from_x/1000.0, move_from_y/1000.0, move_to_x/1000.0, move_to_y/1000.0)
+
+        #x = -588 .. -200		Delta = -388
+        #y = -588 .. -200		Delta = -388
+        
 
     def return_color(self, side):
         if side:
@@ -109,6 +190,21 @@ class Main:
                         rect(ix * 70, iy * 70, 70, 70, (255, 255, 255))
                 tile = self.boolean_flip(tile)
                 text(ix * 70+15, iy * 70+15, self.unicode, self.board.unicode_array()[(ix+iy*8) * 2])
+
+        #Draw side GUI
+        #Home
+        rect(560+40, 0, 100, 50, (200, 160, 160))
+        text(560+40, 0, self.font, "Home")
+        
+        #Open gripper
+        rect(560+40, 100, 100, 50, (200, 160, 160))
+        text(560+40, 100, self.font, "Open Gripper")
+
+        #Close gripper
+        rect(560+40, 200, 100, 50, (200, 160, 160))
+        text(560+40, 200, self.font, "Close Gripper")
+
+
 
     def boolean_flip(self, boolean):
         if boolean:
